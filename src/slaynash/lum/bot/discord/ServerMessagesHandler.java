fde5e0d3f281f9d0ba668dc4ee6d19c61ab89cd6
Slaynash <hugo.flores@etu.univ-lyon1.fr>
2021-02-07 21:58:14 +0100

    [ML] Now printing full stack trace

--- src/slaynash/lum/bot/discord/ServerMessagesHandler.java
+++ src/slaynash/lum/bot/discord/ServerMessagesHandler.java
@@ -1,6 +1,8 @@
 package slaynash.lum.bot.discord;
 
 import java.awt.Color;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
@@ -13,34 +15,45 @@ public class ServerMessagesHandler {
 				event.getAuthor().getName(),
 				event.getMessage().getContentRaw() );
     
-    CommandManager.runAsServer(event);
-    
-    new Thread(() -> {
-    	try {
-    		MelonLoaderScanner.scanLogs(event);
-    	}
-    	catch(Exception e) {
-    		e.printStackTrace();
-    		
-    		String error = "**An error has occured while reading logs:**\n" + e.toString();
-    		
-    		if (error.length() > 1000) {
-        		String[] lines = error.split("\n");
-        		String toSend = "";
-        		int i = 0;
-        		
-	    		while (i < lines.length) {
-	    			if ((toSend + lines[i] + 1).length() > 1000)
-	    				break;
-	    			
-	    			toSend += "\n" + lines[i];
-	    		}
-	    		
-    			event.getChannel().sendMessage(JDAManager.wrapMessageInEmbed(toSend, Color.RED)).queue();
-    		}
-    		else
-    			event.getChannel().sendMessage(JDAManager.wrapMessageInEmbed(error, Color.RED)).queue();
-    	}
-    }).start();
-  }
+		CommandManager.runAsServer(event);
+		
+		new Thread(() -> {
+			try {
+				MelonLoaderScanner.scanLogs(event);
+			}
+			catch(Exception e) {
+				e.printStackTrace();
+				
+				String error = "**An error has occured while reading logs:**\n" + getStackTrace(e);
+				
+				if (error.length() > 1000) {
+					String[] lines = error.split("\n");
+					String toSend = "";
+					int i = 0;
+					
+					while (i < lines.length) {
+						if ((toSend + lines[i] + 1).length() > 1000) {
+							toSend += "...";
+							break;
+						}
+						
+						toSend += "\n" + lines[i];
+					}
+					
+					event.getChannel().sendMessage(JDAManager.wrapMessageInEmbed(toSend, Color.RED)).queue();
+				}
+				else
+					event.getChannel().sendMessage(JDAManager.wrapMessageInEmbed(error, Color.RED)).queue();
+			}
+		}).start();
+  	}
+
+	private static String getStackTrace(Exception e) {
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw);
+
+		e.printStackTrace(pw);
+
+		return sw.toString();
+	}
 }
