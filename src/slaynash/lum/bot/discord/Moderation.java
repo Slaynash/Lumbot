e8febd679feb9d3e2c3942e21d7a09ad5948c0af
rakosi2 <rattila5@hotmail.com>
2021-06-13 14:06:05 -0700

    [mod] NoMic channel giver update with debug

--- src/slaynash/lum/bot/discord/Moderation.java
+++ src/slaynash/lum/bot/discord/Moderation.java
@@ -13,25 +13,30 @@ public class Moderation extends ListenerAdapter {
     private static final HashMap<Long, Long> noMicChannels = new HashMap<>(){{
         put(439093693769711616L /* VRCMG */     ,673955689298788376L /* no-mic */);
         put(600298024425619456L /* emmVRC */    ,682379676106096678L /* no-mic */);
+        put(663449315876012052L /* MelonLoader */,701494833495408640L /* voice-chat */);
     }};
 
     public static final HashMap<Long, Long> lockDownRoles = new HashMap<>(){{
         put(439093693769711616L /* VRCMG */     ,548534015108448256L /* Member */);
         put(600298024425619456L /* emmVRC */    ,600304115972702209L /* Members */);
-        put(600298024425619456L /* MelonLoader */,663462327022256150L /* Member */);
+        put(663449315876012052L /* MelonLoader */,663462327022256150L /* Member */);
     }};
 
     @Override
     public void onGuildVoiceJoin(GuildVoiceJoinEvent event){
-        Long noMicChannel = noMicChannels.get(event.getGuild().getIdLong());
-        if(noMicChannel != null)
-            event.getGuild().getGuildChannelById(noMicChannel).getManager().putPermissionOverride(event.getMember(),EnumSet.of(Permission.VIEW_CHANNEL),null).queue();
+        Long guildID = (event.getGuild().getIdLong());
+        if(noMicChannels.containsKey(guildID)){
+            event.getJDA().getGuildById(760342261967487066L).getTextChannelById(853694950037389343L).sendMessage("<@" + event.getMember().getIdLong() +"> joined a Voice channel in " + guildID).queue();
+            event.getGuild().getGuildChannelById(noMicChannels.get(guildID)).getManager().putPermissionOverride(event.getMember(),EnumSet.of(Permission.VIEW_CHANNEL, Permission.MESSAGE_READ, Permission.MESSAGE_WRITE), null).queue();
+        }
     }
 
     @Override
     public void onGuildVoiceLeave(GuildVoiceLeaveEvent event){
-        Long noMicChannel = noMicChannels.get(event.getGuild().getIdLong());
-        if(noMicChannel != null)
-            event.getGuild().getGuildChannelById(noMicChannel).getManager().removePermissionOverride(event.getMember()).queue();
+        Long guildID = (event.getGuild().getIdLong());
+        if(noMicChannels.containsKey(guildID)){
+            event.getJDA().getGuildById(760342261967487066L).getTextChannelById(853694950037389343L).sendMessage("<@" + event.getMember().getIdLong() +"> left a Voice channel in " + guildID).queue();
+            event.getGuild().getGuildChannelById(noMicChannels.get(guildID)).getManager().removePermissionOverride(event.getMember()).queue();
+        }
     }
 }
