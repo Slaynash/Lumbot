4047d60da339129f05e508f6cf7818ab75aa673d
rakosi2 <rattila5@hotmail.com>
2021-06-16 22:07:40 -0700

    [ML] Update latest ML Version to 0.4.0 part 2

--- src/slaynash/lum/bot/discord/melonscanner/MelonScanner.java
+++ src/slaynash/lum/bot/discord/melonscanner/MelonScanner.java
@@ -405,7 +405,7 @@ public final class MelonScanner {
         context.isMLOutdated = context.mlVersion != null && !(sanitizeInputString(context.mlVersion).equals(latestMLVersionRelease) || sanitizeInputString(context.mlVersion).equals(latestMLVersionBeta));
 
         if (context.isMLOutdatedVRC) {
-            if (context.pre3)
+            if (!context.mlVersion.equals(latestMLVersionRelease))
                 context.embedBuilder.addField(Localization.get("melonscanner.mloutdated.fieldname", context.lang), Localization.getFormat("melonscanner.mloutdated.uptoalpha", context.lang, latestMLVersionBeta, CommandManager.melonLoaderVRCMinDate), false);
             else
                 context.embedBuilder.addField(Localization.get("melonscanner.mloutdated.fieldname", context.lang), Localization.getFormat("melonscanner.mloutdated.upalpha", context.lang, latestMLVersionBeta, CommandManager.melonLoaderVRCMinDate), false);
