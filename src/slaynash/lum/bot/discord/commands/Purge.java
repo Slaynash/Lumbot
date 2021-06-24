7182e7fe49003eeac67328035ea088f37ba3a98a
rakosi2 <rattila5@hotmail.com>
2021-06-23 21:53:52 -0700

    [mod] add try-catch to purge command

--- src/slaynash/lum/bot/discord/commands/Purge.java
+++ src/slaynash/lum/bot/discord/commands/Purge.java
@@ -7,33 +7,38 @@ import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageHistory;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import slaynash.lum.bot.discord.Command;
+import slaynash.lum.bot.discord.ExceptionUtils;
 import slaynash.lum.bot.discord.ServerMessagesHandler;
 
 public class Purge extends Command {
 
     @Override
     protected void onServer(String paramString, MessageReceivedEvent event) {
-        if (event.getAuthor().getId().equals(event.getGuild().getOwnerId()) || !ServerMessagesHandler.checkIfStaff(event))
-            return;
+        try{
+            if (event.getAuthor().getId().equals(event.getGuild().getOwnerId()) || !ServerMessagesHandler.checkIfStaff(event))
+                return;
 
-        String[] params = paramString.split(" ", 2);
+            String[] params = paramString.split(" ", 2);
 
-        Message replied = event.getMessage().getReferencedMessage();
-        if(replied != null){
-            MessageHistory messages = event.getChannel().getHistoryAfter(replied, 100).complete(); //100 is max you can get
-            List<Message> messagelist = new ArrayList<>(messages.getRetrievedHistory());
-            messagelist.add(replied);
-            event.getTextChannel().deleteMessages(messagelist).queue();
+            Message replied = event.getMessage().getReferencedMessage();
+            if(replied != null){
+                MessageHistory messages = event.getChannel().getHistoryAfter(replied, 100).complete(); //100 is max you can get
+                List<Message> messagelist = new ArrayList<>(messages.getRetrievedHistory());
+                messagelist.add(replied);
+                event.getTextChannel().deleteMessages(messagelist).queue();
+            }
+            else if( params.length>1 && params[1].matches("^\\d{1,3}$") ){
+                int count = Integer.parseInt(params[1]);
+                MessageHistory messages = event.getChannel().getHistoryBefore(event.getMessage(), count).complete();
+                List<Message> messagelist = new ArrayList<>(messages.getRetrievedHistory());
+                messagelist.add(event.getMessage());
+                event.getTextChannel().deleteMessages(messagelist).queue();
+            }
+            else 
+                event.getMessage().reply("Command is `l!purge #` or reply to the top message.").queue();
+        } catch(Exception e) {
+            ExceptionUtils.reportException("An error has occured while running purge:", e, event.getTextChannel());
         }
-        else if( params.length>1 && params[1].matches("^\\d{1,3}$") ){
-            int count = Integer.parseInt(params[1]);
-            MessageHistory messages = event.getChannel().getHistoryBefore(event.getMessage(), count).complete();
-            List<Message> messagelist = new ArrayList<>(messages.getRetrievedHistory());
-            messagelist.add(event.getMessage());
-            event.getTextChannel().deleteMessages(messagelist).queue();
-        }
-        else 
-            event.getMessage().reply("Command is `l!purge #` or reply to the top message.").queue();
     }
 
     @Override
