2c80fe2bc5125ec0f796a1f3361708f4b8ce3b19
rakosi2 <rattila5@hotmail.com>
2021-07-04 10:43:42 -0700

    [ML] ah, there is a isnil function

--- src/slaynash/lum/bot/discord/melonscanner/MelonScannerApisManager.java
+++ src/slaynash/lum/bot/discord/melonscanner/MelonScannerApisManager.java
@@ -151,7 +151,7 @@ public class MelonScannerApisManager {
                                 String name = mod.get("name").checkjstring();
                                 System.out.println("[API] Processing mod " + name);
                                 String approvalStatus = "0";
-                                if (mod.get("approvalStatus") != null)
+                                if (mod.get("approvalStatus") != null && !mod.get("approvalStatus").isnil())
                                     approvalStatus = mod.get("approvalStatus").checkjstring();
                                 String version = mod.get("version").checkjstring();
                                 String downloadLink = mod.get("downloadLink") == LuaValue.NIL ? null : mod.get("downloadLink").checkjstring();
