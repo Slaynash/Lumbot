ae1d30a8dcc28c61d6e661c24236d50ec9fdab4a
rakosi2 <rattila5@hotmail.com>
2021-07-11 11:47:53 -0700

    [ML] import ML version# in known errors

--- src/slaynash/lum/bot/discord/melonscanner/MelonLoaderError.java
+++ src/slaynash/lum/bot/discord/melonscanner/MelonLoaderError.java
@@ -45,7 +45,9 @@ public class MelonLoaderError {
         Gson gson = new Gson();
 
         try (Stream<String> lines = Files.lines(new File("melonscannererrors.json").toPath())) {
-            String data = lines.collect(Collectors.joining("\n"));
+            String data = lines.collect(Collectors.joining("\n"))
+                .replace("$MLRelease$", MelonScanner.latestMLVersionRelease)
+                .replace("$MLBeta$", MelonScanner.latestMLVersionBeta);
 
             HashMap<String, JsonElement> filedata = gson.fromJson(data, new TypeToken<HashMap<String, JsonElement>>() {}.getType());
             synchronized (knownUnhollowerErrors) {
