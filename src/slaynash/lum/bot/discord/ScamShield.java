da84a16f2a516d0aef9e4f297f56d303f7fee2a9
rakosi2 <rattila5@hotmail.com>
2021-07-13 09:07:32 -0700

    [S🛡️] catch failed to open dms

--- src/slaynash/lum/bot/discord/ScamShield.java
+++ src/slaynash/lum/bot/discord/ScamShield.java
@@ -101,8 +101,13 @@ public class ScamShield {
                 .setFooter("Received " + suspiciousCount + " naughty points.");
 
             if (event.getGuild().getSelfMember().hasPermission(Permission.BAN_MEMBERS)) {
-                event.getAuthor().openPrivateChannel().flatMap(channel -> channel.sendMessage("You have been automatically banned from " + event.getGuild().getName() +
-                " for phishing."/*If you think that you were falsely banned, you can appeal here (link). */ + " We highly recommend that you change your password immediately.")).complete();
+                try {
+                    event.getAuthor().openPrivateChannel().flatMap(channel -> channel.sendMessage("You have been automatically banned from " + event.getGuild().getName() +
+                    " for phishing."/*If you think that you were falsely banned, you can appeal here (link). */ + " We highly recommend that you change your password immediately.")).complete();
+                }
+                catch (Exception e) {
+                    System.out.println("Failed to open dms with scammer");
+                }
                 event.getMember().ban(1, "Banned by Lum's Scam Shield").complete();
                 embedBuilder.setDescription("User **" + usernameWithTag + "** (*" + userId + "*) was Banned by the Scam Shield");
                 LogCounter.addSSCounter(userId, message, event.getGuild().getId()); // add to status counter
