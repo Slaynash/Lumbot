7a1c01135eab24a0e7b7a1fbf4f5d277c2c18405
Slaynash <slaynash@survival-machines.fr>
2021-09-14 17:17:18 +0200

    [UCBLLIF] Error handling and reporting

--- src/slaynash/lum/bot/discord/slashs/UnivUCBLLIFExoGenerator.java
+++ src/slaynash/lum/bot/discord/slashs/UnivUCBLLIFExoGenerator.java
@@ -1,5 +1,7 @@
 package slaynash.lum.bot.discord.slashs;
 
+
+import java.awt.Color;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -12,6 +14,7 @@ import java.util.Random;
 
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.interactions.InteractionHook;
+import slaynash.lum.bot.discord.JDAManager;
 import slaynash.lum.bot.utils.ExceptionUtils;
 
 public class UnivUCBLLIFExoGenerator {
@@ -31,43 +34,49 @@ public class UnivUCBLLIFExoGenerator {
 
         event.deferReply().queue(success -> interactionhook = success);
 
-        String exo = event.getOptions().get(0).getAsString();
-        String ticket = event.getOptions().size() > 1 ? event.getOptions().get(1).getAsString() : null;
-
-        String subcommandname = event.getSubcommandName();
-
-        if (!subcommandname.equals("create") && !subcommandname.equals("solve")) {
-            interactionhook.sendMessage("Invalid subcommand name: " + event.getSubcommandName()).queue();
-            return;
-        }
-
-        System.out.println("exo: " + exo);
-
-        HttpRequest request = HttpRequest.newBuilder()
-            .GET()
-            .uri(URI.create("https://liris.cnrs.fr/vincent.nivoliers/suzette.php?exo=" + exo + "&query=" + subcommandname + (ticket != null ? ("&ticket=" + ticket) : "")))
-            .setHeader("User-Agent", "LUM Bot")
-            .timeout(Duration.ofSeconds(30))
-            .build();
-
-        String data;
-
         try {
-            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-            data = response.body();
+            String exo = event.getOptions().get(0).getAsString();
+            String ticket = event.getOptions().size() > 1 ? event.getOptions().get(1).getAsString() : null;
+
+            String subcommandname = event.getSubcommandName();
+
+            if (!subcommandname.equals("create") && !subcommandname.equals("solve")) {
+                interactionhook.sendMessage("Invalid subcommand name: " + event.getSubcommandName()).queue();
+                return;
+            }
+
+            System.out.println("exo: " + exo);
+
+            HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create("https://liris.cnrs.fr/vincent.nivoliers/suzette.php?exo=" + exo + "&query=" + subcommandname + (ticket != null ? ("&ticket=" + ticket) : "")))
+                .setHeader("User-Agent", "LUM Bot")
+                .timeout(Duration.ofSeconds(30))
+                .build();
+
+            String data;
+
+            try {
+                HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+                data = response.body();
+            }
+            catch (IOException | InterruptedException e) {
+                ExceptionUtils.reportException("Request failed for from liris.cnrs.fr", e);
+                event.getChannel().sendMessage("Une erreur s'est produite pendant la récupération de l'exercice").queue();
+                return;
+            }
+
+            System.out.println("cnrs returned: " + data);
+
+            String ticketReturned = data.split("\"ticket\": \"", 2)[1].split("\",", 2)[0];
+            String imageDataStr = data.split("\"data\": \"", 2)[1].split("\"}", 2)[0];
+            byte[] imageData = Base64.getDecoder().decode(imageDataStr);
+
+            interactionhook.sendMessage("Ticket: " + ticketReturned).addFile(imageData, "exercice_" + ticketReturned + ".png").queue();
         }
-        catch (IOException | InterruptedException e) {
-            ExceptionUtils.reportException("Request failed for from liris.cnrs.fr", e);
-            event.getChannel().sendMessage("Une erreur s'est produite pendant la récupération de l'exercice").queue();
-            return;
+        catch (Exception e) {
+            ExceptionUtils.reportException("UCBLLIF command failed", e);
+            interactionhook.sendMessageEmbeds(JDAManager.wrapMessageInEmbed("La commande a échouée. Cette erreur a été rapporté aux développeurs.", Color.RED)).queue();
         }
-
-        System.out.println("cnrs returned: " + data);
-
-        String ticketReturned = data.split("\"ticket\": \"", 2)[1].split("\",", 2)[0];
-        String imageDataStr = data.split("\"data\": \"", 2)[1].split("\"}", 2)[0];
-        byte[] imageData = Base64.getDecoder().decode(imageDataStr);
-
-        interactionhook.sendMessage("Ticket: " + ticketReturned).addFile(imageData, "exercice_" + ticketReturned + ".png").queue();
     }
 }
