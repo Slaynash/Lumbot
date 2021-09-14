2e158216dc3b2e1999496441496314036a6849cf
Slaynash <slaynash@survival-machines.fr>
2021-09-14 16:00:07 +0200

    [LIFASR4] correctly reply to interactionhook

--- src/slaynash/lum/bot/discord/slashs/UnivLIFASR4ExerciceGenerator.java
+++ src/slaynash/lum/bot/discord/slashs/UnivLIFASR4ExerciceGenerator.java
@@ -11,11 +11,14 @@ import java.util.Base64;
 import java.util.Random;
 
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.interactions.InteractionHook;
 import slaynash.lum.bot.utils.ExceptionUtils;
 
 public class UnivLIFASR4ExerciceGenerator {
 
     Random rand = new Random();
+
+    InteractionHook interactionhook;
 
     private static final HttpClient httpClient = HttpClient.newBuilder()
         .version(HttpClient.Version.HTTP_2)
@@ -26,7 +29,7 @@ public class UnivLIFASR4ExerciceGenerator {
     public void onCommand(SlashCommandEvent event) {
         // ₁₂₃₄₅₆₇₈₉
 
-        event.deferReply().queue();
+        event.deferReply().queue(success -> interactionhook = success);
 
         String ticket = event.getOptions().size() > 0 ? event.getOptions().get(0).getAsString() : null;
 
@@ -61,6 +64,6 @@ public class UnivLIFASR4ExerciceGenerator {
         String imageDataStr = data.split("\"data\": \"", 2)[1].split("\"}", 2)[0];
         byte[] imageData = Base64.getDecoder().decode(imageDataStr);
 
-        event.reply("").addFile(imageData, "exercice_" + ticket + ".png").queue();
+        interactionhook.sendFile(imageData, "exercice_" + ticket + ".png").queue();
     }
 }
