a107e8ceb97d9e412053be55283b25dfcdd71370
Slaynash <slaynash@survival-machines.fr>
2021-10-15 00:27:04 +0200

    [ML:BTD6] oopsy

--- apiscripts/btd6_inferno.lua
+++ apiscripts/btd6_inferno.lua
@@ -1,10 +1,10 @@
-local apiData = data:getAsJsonObject():entrySet():iterator()
+local apiData = data:getAsJsonArray()
 local mods = {}
 
 for i = 0, apiData:size() - 1, 1 do
     local mod = apiData:get(i)
     local modDetails = mod:get("versions"):get(0)
-    
+
     table.insert(mods, {
         name = mod:get("name"):getAsString(),
         version = mod:getValue():get("readableVersion"):getAsString(),
