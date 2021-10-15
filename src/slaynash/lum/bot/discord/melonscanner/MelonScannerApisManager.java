c75a336b19555bc8f78829251195aef8521bd05e
Slaynash <slaynash@survival-machines.fr>
2021-10-15 07:45:43 +0200

    [ML:BTD6:Kosmic] Use correct API

--- src/slaynash/lum/bot/discord/melonscanner/MelonScannerApisManager.java
+++ src/slaynash/lum/bot/discord/melonscanner/MelonScannerApisManager.java
@@ -63,7 +63,7 @@ public class MelonScannerApisManager {
         MelonScannerApi api;
         apis.add(api = new MelonScannerApi("VRChat", "vrcmg", "https://api.vrcmg.com/v0/mods.json"));
         api.compareUsingHashes = true;
-        apis.add(api = new MelonScannerApi("BloonsTD6", "btd6_inferno", "https://secureservercdn.net/166.62.114.250/emv.548.myftpupload.com/wp-content/uploads/2021/10/API.json_.txt"));
+        apis.add(api = new MelonScannerApi("BloonsTD6", "btd6_inferno", "http://1330studios.com/api-json/"));
         apis.add(api = new MelonScannerApi("BloonsTD6", "btd6_gurrenm4", "https://raw.githubusercontent.com/gurrenm3/MelonLoader-BTD-Mods/main/mods.json"));
         apis.add(api = new MelonScannerApi("Audica", "audica_ahriana", "https://raw.githubusercontent.com/Ahriana/AudicaModsDirectory/main/api.json"));
         apis.add(api = new MelonScannerApi("TheLongDark", "tld", "https://tld.xpazeapps.com/api.json"));
