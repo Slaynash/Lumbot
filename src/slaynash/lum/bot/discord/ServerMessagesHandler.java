557128d48796b2ec6573647a17fe5e83a26a04eb
Slaynash <hugo.flores@etu.univ-lyon1.fr>
2021-12-27 00:25:42 +0100

    [DM] Fix echo

--- src/slaynash/lum/bot/discord/ServerMessagesHandler.java
+++ src/slaynash/lum/bot/discord/ServerMessagesHandler.java
@@ -34,7 +34,10 @@ public class ServerMessagesHandler {
 
     public static void mainHandle(MessageReceivedEvent event) {
         try {
-            if (event.getGuild().getIdLong() == 633588473433030666L /* Slaynash's Workbench */ && event.getChannel().getName().toLowerCase().startsWith("dm-")) {
+            if (event.getAuthor().getIdLong() != event.getJDA().getSelfUser().getIdLong() &&
+                event.getGuild().getIdLong() == 633588473433030666L /* Slaynash's Workbench */ &&
+                event.getChannel().getName().toLowerCase().startsWith("dm-"))
+            {
                 JDAManager.getJDA().getUserById(event.getChannel().getName().split("-")[1]).openPrivateChannel().queue(channel -> {
                     channel.sendMessage(event.getMessage()).queue();
                 }, error -> {
