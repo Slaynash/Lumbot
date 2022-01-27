9abedb761b788fe3348aacbe61e4b1e37329b2e9
rakosi2 <rattila5@hotmail.com>
2022-01-27 14:48:46 -0800

    [ML:UNO] woopies

--- apiscripts/uno.lua
+++ apiscripts/uno.lua
@@ -3,13 +3,12 @@ local mods = {}
 
 for i = 0, apiData:size() - 1, 1 do
     local mod = apiData:get(i)
-    local modDetails = mod:get("Versions"):get(0)
 
     table.insert(mods, {
         name = mod:get("Name"):getAsString(),
-        version = modDetails:get("Version"):getAsString(),
-        downloadLink = modDetails:get("DownloadLink"):getAsString(),
-        hash = modDetails:get("Hash"):getAsString()
+        version = mod:get("Version"):getAsString(),
+        downloadLink = mod:get("DownloadLink"):getAsString(),
+        hash = mod:get("Hash"):getAsString()
     })
 end
 
