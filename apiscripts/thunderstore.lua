6872893bf2f6350d6dde6124c71c9121133da28b
rakosi2 <rattila5@hotmail.com>
2022-07-02 15:55:47 -0700

    [CICD] add 2 files to be uploaded

--- apiscripts/thunderstore.lua
+++ apiscripts/thunderstore.lua
@@ -5,13 +5,13 @@ for i = 0, apiData:size() - 1, 1 do
     local mod = apiData:get(i)
     if mod:get("full_name"):getAsString() == "LavaGang-MelonLoader" then goto continue end
 
-    is_deprecated = mod:get("is_deprecated"):getAsBoolean()
     local modDetails = mod:get("versions"):get(0)
 
     table.insert(mods, {
         name = modDetails:get("name"):getAsString(),
         version = modDetails:get("version_number"):getAsString(),
-        downloadLink = modDetails:get("download_url"):getAsString()
+        downloadLink = modDetails:get("download_url"):getAsString(),
+        is_deprecated = mod:get("is_deprecated"):getAsBoolean()
     })
     ::continue::
 end
