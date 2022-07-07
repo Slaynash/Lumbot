7b59fbc0cc792eee1dcee8a3e5a6bb99000c06bf
Slaynash <hugo.flores@etu.univ-lyon1.fr>
2022-07-08 01:40:03 +0200

    [ML:API:CF] I can't Lua (fix typo)

--- apiscripts/curseforge.lua
+++ apiscripts/curseforge.lua
@@ -10,9 +10,9 @@ for i = 0, apiData:size() - 1, 1 do
     -- CurseForge doesn't have a proper mod versioning, so we have to use the file name instead
     local rawfilename = latestFiles:get("fileName"):getAsString()
     -- remove the file extension
-    local filenameWithoutExt = string:gsub(rawfilename, "%.[^%.]*$", "")
+    local filenameWithoutExt = string.gsub(rawfilename, "%.[^%.]*$", "")
     -- get only the version from the filename (can have a "v", and be preceded by a space, "-", or "_")
-    local version = string:gsub(filenameWithoutExt, "^(.*?)(v?)[-_ ][^%d]*", "")
+    local version = string.gsub(filenameWithoutExt, "^(.*?)(v?)[-_ ][^%d]*", "")
 
     table.insert(mods, {
         name = mod:get("name"):getAsString(),
