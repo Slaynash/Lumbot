e46616955d96d2278709ba7b4edc8b83401540fb
STBlade <dommrogers@gmail.com>
2024-02-13 22:10:04 +0000

    fix compareVersion 2.06 condition

--- src/slaynash/lum/bot/discord/melonscanner/MelonScanner.java
+++ src/slaynash/lum/bot/discord/melonscanner/MelonScanner.java
@@ -420,7 +420,7 @@ public final class MelonScanner {
             }
         }
         else if (context.game.equalsIgnoreCase("TheLongDark")) {
-            if (context.gameBuild != null && VersionUtils.compareVersion("2.06", context.gameBuild) >= 0) {
+            if (context.gameBuild != null && VersionUtils.compareVersion("2.06", context.gameBuild) <= 0) {
 		if (VersionUtils.compareVersion("0.6.2", context.mlVersion) == 0) {
                     context.embedBuilder.addField("TLD ML062", "MelonLoader 0.6.2 causes issues with TheLongDark mods, Please downgrade to MelonLoader 0.6.1 using the installer by unticking \"latest\"", false);
 		}
