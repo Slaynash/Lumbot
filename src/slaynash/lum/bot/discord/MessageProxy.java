Slaynash/Lumbot

commit 65f7a0340ff4239b51f508681e590563abfe1fa6

[Replies] handle replies from MelonScanner in DMs

src/slaynash/lum/bot/discord/MessageProxy.java
@@ -392,16 +392,15 @@ public class MessageProxy {
         return false;
     }
 
-    private static void handleDMReplies(MessageReceivedEvent event, String content) {
+    public static boolean handleDMReplies(MessageReceivedEvent event, String content) {
         try {
             if (content == null || content.isBlank())
-                return;
+                return false;
             content = content.toLowerCase();
             if (event.getAuthor().equals(event.getJDA().getSelfUser()))
-                return;
-            String guildID = "0";
+                return false;
             try {
-                ResultSet rs = DBConnectionManagerLum.sendRequest("SELECT * FROM `Replies` WHERE `guildID` = ?", guildID);
+                ResultSet rs = DBConnectionManagerLum.sendRequest("SELECT * FROM `Replies` WHERE `guildID` = 0");
 
                 while (rs.next()) {
                     int ukey = rs.getInt("ukey");
@@ -468,8 +467,12 @@ public class MessageProxy {
                         event.getMessage().reply(message).queue();
                         Guild mainGuild = JDAManager.getJDA().getGuildById(JDAManager.mainGuildID);
                         if (mainGuild == null)
-                            return;
+                            return true;
                         TextChannel guildchannel = mainGuild.getTextChannels().stream().filter(c -> c.getName().contains(event.getAuthor().getId())).findFirst().orElse(null);
+                        if (guildchannel == null) {
+                            System.out.println("Creating DM Channel " + event.getAuthor().getId());
+                            guildchannel = mainGuild.createTextChannel("dm-" + event.getAuthor().getEffectiveName() + "-" + event.getAuthor().getId(), mainGuild.getCategoryById(924780998124798022L)).complete();
+                        }
                         guildchannel.sendMessage(message).setAllowedMentions(Arrays.asList(MentionType.USER, MentionType.ROLE)).queue();
                     }
                     if (report) {
@@ -493,8 +496,8 @@ public class MessageProxy {
                         }
                     }
                 }
-
                 DBConnectionManagerLum.closeRequest(rs);
+                return true;
             }
             catch (SQLException e) {
                 ExceptionUtils.reportException("Failed to check replies", e, event.getChannel());
@@ -503,5 +506,6 @@ public class MessageProxy {
         catch (Exception e) {
             e.printStackTrace();
         }
+        return false;
     }
 }

src/slaynash/lum/bot/discord/ServerMessagesHandler.java
@@ -314,6 +314,8 @@ public class ServerMessagesHandler {
         try {
             if (content == null || content.isBlank())
                 return false;
+            if (event.getChannelType() == ChannelType.PRIVATE) // Possible to get here from MelonScanner in DMs
+                return MessageProxy.handleDMReplies(event, content);
             content = content.toLowerCase();
             if (event.getAuthor().equals(event.getJDA().getSelfUser()))
                 return true;

commit e62d3215a7fe71d64e9202e7bcbf6092da85e457

[DM] fix log scans broken by 65f7a03

src/slaynash/lum/bot/discord/MessageProxy.java
@@ -407,6 +407,7 @@ public class MessageProxy {
             try {
                 ResultSet rs = DBConnectionManagerLum.sendRequest("SELECT * FROM `Replies` WHERE `guildID` = 0");
 
+                boolean found = false;
                 while (rs.next()) {
                     int ukey = rs.getInt("ukey");
                     String regex = rs.getString("regex");
@@ -471,8 +472,10 @@ public class MessageProxy {
                     else if (message != null && !message.isBlank()) {
                         event.getMessage().reply(message).queue();
                         Guild mainGuild = JDAManager.getJDA().getGuildById(JDAManager.mainGuildID);
-                        if (mainGuild == null)
+                        if (mainGuild == null) {
+                            System.out.println("Main guild not found");
                             return true;
+                        }
                         TextChannel guildchannel = mainGuild.getTextChannels().stream().filter(c -> c.getName().contains(event.getAuthor().getId())).findFirst().orElse(null);
                         if (guildchannel == null) {
                             System.out.println("Creating DM Channel " + event.getAuthor().getId());
@@ -480,6 +483,7 @@ public class MessageProxy {
                         }
                         guildchannel.sendMessage(message).setAllowedMentions(Arrays.asList(MentionType.USER, MentionType.ROLE)).queue();
                     }
+                    found = true;
                     if (report) {
                         MessageChannelUnion reportChannel = CommandManager.getModReportChannels(event, "reply");
                         if (reportChannel != null) {
@@ -502,7 +506,7 @@ public class MessageProxy {
                     }
                 }
                 DBConnectionManagerLum.closeRequest(rs);
-                return true;
+                return found;
             }
             catch (SQLException e) {
                 ExceptionUtils.reportException("Failed to check replies", e, event.getChannel());